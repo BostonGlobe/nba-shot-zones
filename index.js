33898c6d8c2b91aa10dc26078813d1ba8f52529f
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-17 16:12:50 -0400

    variables

--- index.js
+++ index.js
@@ -1,73 +1,82 @@
-// assumes 0,0 is at hoop center
+// assumes origin [0,0] is hoop center
+
+const restricted = 4
+const freeThrowLine = 13.75
+const paint = 16
+const cornerThreeX = 22
+const cornerThreeY = 8.75
+const three = 23.75
+const deepThree = 28
+
 const zones = [
 	{ name: 'restricted area', contains: ({ d }) =>
-		d <= 4,
+		d <= restricted,
 	},
 	{ name: 'low post', contains: ({ x, y, d }) =>
-		d > 4 &&
-		y <= 6.875 &&
-		Math.abs(x) <= 8,
+		d > restricted &&
+		y <= (freeThrowLine / 2) &&
+		Math.abs(x) <= (paint / 2),
 	},
 	{ name: 'high post', contains: ({ x, y, d }) =>
-		y > 6.875 &&
-		y < 13.75 &&
-		Math.abs(x) <= 8,
+		y > (freeThrowLine / 2) &&
+		y < freeThrowLine &&
+		Math.abs(x) <= (paint / 2),
 	},
 	{ name: 'midrange (lower right)', contains: ({ x, y, d }) =>
-		x >= -22 &&
-		x < -8 &&
-		y < 8.75,
+		x >= -cornerThreeX &&
+		x < -(paint / 2) &&
+		y < cornerThreeY,
 	},
 	{ name: 'midrange (lower left)', contains: ({ x, y, d }) =>
-		x > 8 &&
-		x <= 22 &&
-		y < 8.75,
+		x > (paint / 2) &&
+		x <= cornerThreeX &&
+		y < cornerThreeY,
 	},
 	{ name: 'midrange (upper right)', contains: ({ x, y, d }) =>
-		x >= -22 &&
-		x < -8 &&
-		y >= 8.75 &&
-		d <= 23.75,
+		x >= -cornerThreeX &&
+		x < -(paint / 2) &&
+		y >= cornerThree &&
+		d <= three,
 	},
 	{ name: 'midrange (upper left)', contains: ({ x, y, d }) =>
-		x > 8 &&
-		x <= 22 &&
-		y >= 8.75 &&
-		d <= 23.75,
+		x > (paint / 2) &&
+		x <= cornerThreeX &&
+		y >= cornerThree &&
+		d <= three,
 	},
 	{ name: 'midrange (upper middle)', contains: ({ x, y, d }) =>
-		Math.abs(x) <= 8 &&
-		y > 13.75 &&
-		d <= 23.75,
+		Math.abs(x) <= paint &&
+		y > freeThrowLine &&
+		d <= three,
 	},
 	{ name: 'three (right corner)', contains: ({ x, y }) =>
-		x < -22 &&
-		y < 8.75,
+		x < -cornerThreeX &&
+		y < cornerThreeY,
 	},
 	{ name: 'three (left corner)', contains: ({ x, y}) =>
-		x > 22 &&
-		y < 8.75,
+		x > cornerThreeX &&
+		y < cornerThreeY,
 	},
 	{ name: 'three (right)', contains: ({ x, y, d }) =>
-		d > 23.75 &&
-		d < 28 &&
-		x < -8 &&
-		y > 8.75,
+		d > three &&
+		d < deepThree &&
+		x < -paint &&
+		y > cornerThreeY,
 	},
 	{ name: 'three (middle)', contains: ({ x, y, d }) =>
-		d > 23.75 &&
-		d < 28 &&
-		Math.abs(x) <= 8 &&
-		y > 8.75,
+		d > three &&
+		d < deepThree &&
+		Math.abs(x) <= paint &&
+		y > cornerThreeY,
 	},
 	{ name: 'three (left)', contains: ({ x, y, d }) =>
-		d > 23.75 &&
-		d < 28 &&
-		x > 8 &&
-		y > 8.75,
+		d > three &&
+		d < deepThree &&
+		x > paint &&
+		y > cornerThreeY,
 	},
 	{ name: 'three (deep)', contains: () => ({ d }) =>
-		d >= 28
+		d >= deepThree
 	},
 ]
 
